08f88036d008c9b94dafb27fcbc13a4f134b50de
antonio93hb <antonio93hb@hotmail.com>
2025-05-27 17:15:41 +0200

    Initial Commit

---
+++ MuseumApp/MuseumAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuseumAppApp.swift
+//  MuseumApp
+//
+//  Created by Antonio Hernández Barbadilla on 27/5/25.
+//
+
+import SwiftUI
+
+@main
+struct MuseumAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
